f424279908935b6a17c80d5be428e8dbea0e9036
yelmasry <yarra.elmasry@gmail.com>
2025-02-12 01:22:20 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,37 +6,48 @@ class ApplicationController < ActionController::Base
 
   ###Square
   def square_new
+    render({ :template => "application_templates/square_new"})
+  end 
+
+  def square_new_results
     @the_num = params.fetch("user_number").to_f
 
     @the_result = @the_num ** 2
   
-    render({ :template => "application_templates/square_new"})
+    render({ :template => "application_templates/square_new_result"})
   end 
+
 
 
   ###Square Root
   def square_root_new
-    @the_num = params.fetch("users_number").to_f
+    render({ :template => "application_templates/square_root_new"})
+  end 
+  
+  def square_root_results
+    @the_num = params.fetch("user_number").to_f
 
     @the_result = @the_num ** 0.5
 
-    render({ :template => "application_templates/square_root_new"})
+    render({ :template => "application_templates/square_root_results"})
   end 
 
 
   ###Payment
-  def payment_new 
+  def payment_new
+    render({ :template => "application_templates/payment_new"})
+  end 
+  
+  def payment_new_results
     @apr = ((params.fetch("APR_user").to_f) / 100 / 12 )    
   
     @years = (params.fetch("years_user")).to_i * 12 
   
     @principal = (params.fetch("principal_user")).to_f 
   
-  
     @formatted_apr = @apr.to_fs(:percentage, { :precision => 4 })
   
     @formatted_principal = @principal.to_fs(:currency)
-  
   
     @numerator = @apr * @principal 
   
@@ -46,18 +57,22 @@ class ApplicationController < ActionController::Base
   
     @formatted_present_value = @present_value.to_fs(:currency)
     
-    render({ :template => "application_templates/payment_new"})
+    render({ :template => "application_templates/payment_new_results"})
   end 
 
 
   ###Random Number 
   def random_new 
+    render({ :template => "application_templates/random_new"})
+  end 
+  
+  def random_new_results 
     @min = params[:min].to_f
     @max = params[:max].to_f
   
     @random_number = rand(@min..@max)
 
-    render({ :template => "application_templates/random_new"})
+    render({ :template => "application_templates/random_new_results"})
   end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,27 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "home" })
 
-  get("/:square/:new", { :controller => "application", :action => "square_new"})
+  get("/square/new", { :controller => "application", :action => "square_new"})
 
-  get("/:square_root/:new", { :controller => "application", :action => "square_root_new"})
+  get("/square/results", { :controller => "application", :action => "square_new_results"})
 
-  get("/:payment/:new", { :controller => "application", :action => "payment_new"})
+  
+  get("/square_root/new", { :controller => "application", :action => "square_root_new"})
 
-  get("/:random/:new", { :controller => "application", :action => "random_new"})
+  get("/square_root/results", { :controller => "application", :action => "square_root_results"})
+
+  
+  
+  get("/payment/new", { :controller => "application", :action => "payment_new"})
+  
+  get("/payment/results", { :controller => "application", :action => "payment_new_results"})
+
+
+
+
+  get("/random/new", { :controller => "application", :action => "random_new"})
+
+  get("/random/results", { :controller => "application", :action => "random_new_results"})
+
 
 end
